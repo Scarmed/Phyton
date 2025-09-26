707a243cbe36f6fe2f0ef44c7567cb9b287c5049
Sebastian <scarmed1601@ieszaidinvergeles.org>
2025-09-26 14:13:21 +0200

    calulocuadrado

---
+++ CalculoCuadrado.py
@@ -0,0 +1,6 @@
+bas=input('Dame la longitud de la base: ')
+alt=input('dame la al alutra: ')
+area= int(bas)*int(alt)
+perim= (int(bas)+int(alt))*2
+print('el area del cuadrado es:',area)
+print('el perimetro del cuadrado es:',perim)
---
+++ entradaslida.py
@@ -0,0 +1,4 @@
+numero1=input("introduce un numero: ")
+numero2= input('dime otro numero: ')
+resultado = int(numero2) + int(numero1)
+print(resultado)
