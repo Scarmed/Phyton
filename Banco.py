d9a9c69fda551aa5471960c60cf617dc3d329216
Sebastian <scarmed1601@ieszaidinvergeles.org>
2025-10-06 11:55:43 +0200

    ejercicio2

---
+++ Banco.py
@@ -0,0 +1,3 @@
+capital= input('dime cuanto dinero quieres inventir: ')
+tasa = input('Dime cuantos intereses hay actualmente para calcular las ganancias: ')
+tiempo = input('Dime cuanto tiempo va a pasar desde esta transaccion: ')
---
+++ IndiceDeMasa.py
@@ -0,0 +1,4 @@
+peso=int(input("dime tu peso: "))
+altura=float(input("dime tu altura: "))
+Indice= peso/(altura)**2
+print("tu IMC es de: ",round(Indice,2))
