95c2cf83eec884752d5043fd93d2e0786a77e2db
Sebastian <scarmed1601@ieszaidinvergeles.org>
2025-10-09 09:41:52 +0200

    Lista Ejerecicio 10

---
+++ Ejercicio10.py
@@ -0,0 +1,8 @@
+payaso = input("Cuantos payasos se han comprado: ")
+muñecas = input("Cuantas muñecas se han comprado: ")
+pesoP = 112
+pesoM = 75 
+TotalP = int(payaso) * int(pesoP)
+TotalM = int(muñecas) * int(pesoM)
+PesoTotal = int(TotalP) + int(TotalP)
+print("El peso total de las ventas de la ultima semana es: ",PesoTotal)
