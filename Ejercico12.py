9e22fa67d5db574ad0007c9718a3686b60728ec9
Sebastian <scarmed1601@ieszaidinvergeles.org>
2025-10-09 10:38:11 +0200

    Lista Ejercicio 12

--- Ejercico12.py
+++ Ejercico12.py
@@ -1,5 +1,5 @@
 PanDia = 3.49
-PanNoDia = float(PanDia)*0.6
+PanNoDia = float(PanDia)*0.4
 BarrasVendidasNoDia = input("Cuantas barras de hace dias se vendieron: ")
 TotalVendidoNoDia = PanNoDia * int(BarrasVendidasNoDia)
-print("El precio de las barras de hoy es: ",PanDia,"pero el de ayer tiene un descuento del 60% quedando con un costo de:",PanNoDia,"y se vendio un total de las ventas de pan del dia de ayer",round(TotalVendidoNoDia,2))
+print("El precio de las barras de hoy es: ",PanDia,"pero el de ayer tiene un descuento del 60% quedando con un costo de:",PanNoDia,"y se vendio un total de pan del dia de ayer",round(TotalVendidoNoDia,2))
