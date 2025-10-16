954bfbf825657bab6b6f3d776c2ab47c58c71429
Sebastian <scarmed1601@ieszaidinvergeles.org>
2025-10-16 09:43:59 +0200

    Cadena Ejercicio 3

---
+++ CadenasEj3.py
@@ -0,0 +1,3 @@
+nombre = input('Dime tu nombre: ')
+total_letras = len(nombre)
+print(nombre,'tiene',total_letras,'letras,donde',nombre.upper(),'es el nombre de usario en mayusculas y',total_letras,'es el nummero de letras que tiene el nombre')
