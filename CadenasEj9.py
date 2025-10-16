f0870679498b0278a7467b051eebe8585952540b
Sebastian <scarmed1601@ieszaidinvergeles.org>
2025-10-16 10:50:00 +0200

    Cadena Ejercicio 9

---
+++ CadenasEj9.py
@@ -0,0 +1,7 @@
+fechaNacimiento = input('Dime cuando naciste con el formato dd/mm/aaaa: ')
+Dia = fechaNacimiento.split('/')[0]
+Mes = fechaNacimiento.split('/')[1]
+Año = fechaNacimiento.split('/')[2]
+print(Dia)
+print(Mes)
+print(Año)
