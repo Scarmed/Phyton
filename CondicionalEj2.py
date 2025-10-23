10ef6925518e6dd0aeffa70a4360779164889f1e
Sebastian <scarmed1601@ieszaidinvergeles.org>
2025-10-23 09:57:09 +0200

    Condicional Ejercicio 2

---
+++ CondicionalEj2.py
@@ -0,0 +1,6 @@
+Contraseña = '#Drk1216_'
+DimeContraseña = input("Dime la contraseña del dispositivo: ")
+if(Contraseña != DimeContraseña):
+    print("La contraseña no coincide con la que has escrito")
+else:
+    print("Perfecto la contraseña",Contraseña,"y la que tu has escrtio",DimeContraseña,"Coinciden")
