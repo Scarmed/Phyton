a7ac6e4b4a22922a786ae94a45567ec80343ee3c
Sebastian <scarmed1601@ieszaidinvergeles.org>
2025-10-23 10:21:44 +0200

    Condicionales Ejercicio 3

---
+++ CondicionalEj3.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Dime un numero: "))
+numero2 = int(input("Dime un segundo numero: "))
+if (numero2 == 0):
+     print("error")
+   
+else:
+    Resultado = int(numero1) / int(numero2)
+    print("El resultado da: ",Resultado)
